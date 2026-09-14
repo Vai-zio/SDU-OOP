b67f0ec4ca613167407bea8f60b7949767bfb19c
Vai-zio <villadskbreumlund@gmail.com>
2026-09-14 13:31:10 +0200

    Added Christmas Sale as a project

---
+++ Christmas sale/program.cs
@@ -0,0 +1,20 @@
+﻿Random rnd = new Random();
+int a = 21816000;
+double price = 599.95;
+bool IsItChristmas = false;
+int Dec = 0;
+int i = 0;
+
+while(i < 10) {
+    int rndtime = rnd.Next(1,13);
+    int time = (a/12)*rndtime;
+    Dec = a - (a / 12);
+    if(time >= Dec) { 
+        IsItChristmas = true;
+        Console.WriteLine("The new price would then equivulate to :" +(price*0.7));
+        }
+    Console.WriteLine("IsItChristmas: "+IsItChristmas);
+    Console.WriteLine("The generated month was the: "+rndtime);
+    Console.WriteLine("");
+    i++;
+}
