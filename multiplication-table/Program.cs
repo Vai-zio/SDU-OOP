1460bd1b75b3e4ed2f336159b2aaf081d7005cef
Vai-zio <villadskbreumlund@gmail.com>
2026-09-15 15:40:30 +0200

    Multiplication table print out (3 table)

---
+++ multiplication-table/Program.cs
@@ -0,0 +1,15 @@
+﻿Random rnd = new Random();
+
+int size = rnd.Next(1,31);
+int[] array = new int[size];
+for (int i = 0; i < size; i++) 
+{
+    try {
+        array[i] = 3*i;
+        Console.WriteLine(array[i]);
+}   catch(IndexOutOfRangeException) {
+        Console.WriteLine("Oopsie!"); } 
+}
+Console.WriteLine("Array size is: "+size);
+
+//Note for some reason it doesn't write the last one, don't know why
