02fa2012f659d65f1f8a629cd91f4678ce351ad9
Vai-zio <villadskbreumlund@gmail.com>
2026-09-16 11:42:34 +0200

    Area of circles (r*pi)

---
+++ Area of circles/Program.cs
@@ -0,0 +1,5 @@
+﻿double pi = 3.141592;
+int[] radius = {1,3,5};
+for(int i = 0; i < radius.Length; i++) {
+    Console.WriteLine("The radius of circle "+(i+1)+" is equal to: "+((double)pi*radius[i]));
+}
